571c5e3a3f97248ff06445532871bde23b6f3100
Ronnie Hansted Olsen <ronn5410@edu.zealand.dk>
2023-12-20 03:37:04 +0100

    Final Changes

--- WebApplication1/Pages/Profile/UpdateAccount.cshtml.cs
+++ WebApplication1/Pages/Profile/UpdateAccount.cshtml.cs
@@ -1,89 +1,89 @@
-using Azure.Identity;
-using foodzcore.Models;
-using foodzcore.Pages.Login;
-using foodzcore.Services.AccountServices;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Azure.Identity;
+//using foodzcore.Models;
+//using foodzcore.Pages.Login;
+//using foodzcore.Services.AccountServices;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace foodzcore.Pages.Profile
-{
-    public class UpdateAccountModel : PageModel
-    {
-        private readonly AccountUpdateService _accountUpdateService;
+//namespace foodzcore.Pages.Profile
+//{
+//    public class UpdateAccountModel : PageModel
+//    {
+//        private readonly AccountUpdateService _accountUpdateService;
 
-        public UpdateAccountModel(AccountUpdateService accountUpdateService)
-        {
-            _accountUpdateService = accountUpdateService;
-        }
+//        public UpdateAccountModel(AccountUpdateService accountUpdateService)
+//        {
+//            _accountUpdateService = accountUpdateService;
+//        }
 
-        public Account AccountItem { get; set; }
+//        public Account AccountItem { get; set; }
 
-        //[BindProperty]
-        //public string Username { get; set; }
+//        //[BindProperty]
+//        //public string Username { get; set; }
 
-        //[BindProperty]
-        //public string Email { get; set; }
-
-
-        //[BindProperty]
-        //public Account ActiveUser { get; set; }
+//        //[BindProperty]
+//        //public string Email { get; set; }
 
 
-        public void OnGet()
-        {
-            // Retrieves the logged-in user information from LoginModel
-            AccountItem = LoginModel.LoggedInUser;
-            Console.WriteLine(AccountItem.Username);
-            Console.WriteLine(AccountItem.Email);
-            Console.WriteLine(AccountItem.UserID);
-            Console.WriteLine(AccountItem.Password);
-            Console.WriteLine(AccountItem.ConfirmPassword);
-        }
+//        //[BindProperty]
+//        //public Account ActiveUser { get; set; }
 
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                Console.WriteLine("Model State Errors:");
+//        public void OnGet()
+//        {
+//            // Retrieves the logged-in user information from LoginModel
+//            AccountItem = LoginModel.LoggedInUser;
+//            Console.WriteLine(AccountItem.Username);
+//            Console.WriteLine(AccountItem.Email);
+//            Console.WriteLine(AccountItem.UserID);
+//            Console.WriteLine(AccountItem.Password);
+//            Console.WriteLine(AccountItem.ConfirmPassword);
+//        }
 
-                foreach (var modelState in ModelState.Values)
-                {
-                    foreach (var error in modelState.Errors)
-                    {
-                        Console.WriteLine($"- {error.ErrorMessage}");
-                    }
-                }
-                Console.WriteLine(AccountItem.Username);
-                Console.WriteLine(AccountItem.Email);
-                Console.WriteLine(AccountItem.UserID);
-                Console.WriteLine(AccountItem.Password);
-                Console.WriteLine(AccountItem.ConfirmPassword);
-                Console.WriteLine("NOPE");
-                return Page();
-            }
 
-            Console.WriteLine(AccountItem.Username);
-            Console.WriteLine(AccountItem.Email);
-            Console.WriteLine(AccountItem.UserID);
-            Console.WriteLine(AccountItem.Password);
-            Console.WriteLine(AccountItem.ConfirmPassword);
-            Console.WriteLine("YEAH");
+//        public async Task<IActionResult> OnPostAsync()
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                Console.WriteLine("Model State Errors:");
 
-            var updateResult = await _accountUpdateService.UpdateAccountAsync(AccountItem);
+//                foreach (var modelState in ModelState.Values)
+//                {
+//                    foreach (var error in modelState.Errors)
+//                    {
+//                        Console.WriteLine($"- {error.ErrorMessage}");
+//                    }
+//                }
+//                Console.WriteLine(AccountItem.Username);
+//                Console.WriteLine(AccountItem.Email);
+//                Console.WriteLine(AccountItem.UserID);
+//                Console.WriteLine(AccountItem.Password);
+//                Console.WriteLine(AccountItem.ConfirmPassword);
+//                Console.WriteLine("NOPE");
+//                return Page();
+//            }
 
-            if (updateResult)
-            {
-                // Redirect to the Userprofile details page after successful update
-                return RedirectToPage("/UserProfile");
-            }
-            else
-            {
-                // Update Failed, return to same page
-                return Page();
-            }
+//            Console.WriteLine(AccountItem.Username);
+//            Console.WriteLine(AccountItem.Email);
+//            Console.WriteLine(AccountItem.UserID);
+//            Console.WriteLine(AccountItem.Password);
+//            Console.WriteLine(AccountItem.ConfirmPassword);
+//            Console.WriteLine("YEAH");
 
-        }
-    }
-}
+//            var updateResult = await _accountUpdateService.UpdateAccountAsync(AccountItem);
+
+//            if (updateResult)
+//            {
+//                // Redirect to the Userprofile details page after successful update
+//                return RedirectToPage("/UserProfile");
+//            }
+//            else
+//            {
+//                // Update Failed, return to same page
+//                return Page();
+//            }
+
+//        }
+//    }
+//}
 
